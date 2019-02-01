7c44b74f9fa53863ad3176ac053a4a130f4b25b8
Daniel Cosme <cosmedaniel8@gmail.com>
2019-02-01 01:14:15 -0500

    + Node system to manipulate enemy movement as desired.
    Needs polishing but I'm confident Is modular and should not conflict against other scripts.

---
+++ Child's Play GE2 Project 2019/Assets/Scripts/Map/Nodes/DeterminedNodeSelector.cs
@@ -0,0 +1,32 @@
+﻿using FromOutside;
+#if UNITY_EDITOR
+using UnityEngine;
+#endif
+
+namespace Map.Nodes
+{
+    /// <summary>
+	/// Selects the next node in the order it appears on the list.
+	/// </summary>
+    public class DeterminedNodeSelector : AbstractNodeSelector
+    {
+        /// <summary>
+		/// Index to keep track of what node should be selected next
+		/// </summary>
+        protected int nextNode;
+
+        /// <summary>
+		/// Selects the next node to travel to.
+		/// </summary>
+		/// <returns>The next selected node, or null if there are no valid nodes</returns>
+        public override Node GetNextNode()
+        {
+            if (listOfNodes.Next(ref nextNode, true))
+            {
+                return listOfNodes[nextNode];
+            }
+            return null;
+        }
+    }
+}
+
---
+++ Child's Play GE2 Project 2019/Assets/Scripts/Map/Nodes/RandomNodeSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map.Nodes
+{
+    public class RandomNodeSelector : AbstractNodeSelector
+    {
+        public override Node GetNextNode()
+        {
+            throw new System.NotImplementedException();
+        }
+
+   
+    }
+}
+
