2db224b2ccc5a5eb485d7550f7cec32f3d08630e
GrisWoldDiablo <griswoldd@hotmail.com>
2019-01-31 19:04:48 -0500

    +*(Alex) Tile proper, Place Item, Remove Item, 2 models
    got the scaling of the tiles worked out, 5x5 units, placed a prefab with the correct proportions and made a small demo path in my scene, proper rotation alignments when placing items and differentiate between path tiles and tower tiles.

--- Child's Play GE2 Project 2019/Assets/_Alex/Scripts/PlaceItem.cs
+++ Child's Play GE2 Project 2019/Assets/_Alex/Scripts/PlaceItem.cs
@@ -5,44 +5,101 @@ using UnityEngine;
 public class PlaceItem : MonoBehaviour
 {
     private Camera cam;
-    [SerializeField] private GameObject itemPrefabTEST;
-    private GameObject itemSelected;
+    [SerializeField] private GameObject itemPrefabTestTower;
+    [SerializeField] private GameObject itemPrefabTestPath;
+    private GameObject itemSelectedTower;
+    private GameObject itemSelectedPath;
     // Start is called before the first frame update
     void Start()
     {
-        itemSelected = Instantiate(itemPrefabTEST);
-        itemSelected.SetActive(false);
+        itemSelectedTower = Instantiate(itemPrefabTestTower);
+        itemSelectedTower.GetComponent<Item>().RangeGO.SetActive(true);
+        itemSelectedTower.SetActive(false);
+
+        itemSelectedPath = Instantiate(itemPrefabTestPath);
+        itemSelectedPath.SetActive(false);
         cam = GetComponent<Camera>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+        PlaceItemHit();
+    }
+    // Variable for PlaceItemHit Method
+    private ItemTile itemTileScriptForHit;
+    private RaycastHit hitForTile;
+    /// <summary>
+    /// Shoots a ray and detect if the 
+    /// </summary>
+    private void PlaceItemHit()
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitForTile))
         {
 
-            GameObject hitGO = hit.collider.gameObject;
+            GameObject hitGO = hitForTile.collider.gameObject;
             Debug.Log(hitGO);
-            if (hitGO.CompareTag("Tile"))
+            if (hitGO.CompareTag("TileTower"))
             {
-                itemSelected.SetActive(true);
-                itemSelected.transform.position = hitGO.transform.position;
-                if (Input.GetButton("Fire1"))
-                {
-                    ItemTile itemTileScript = hit.collider.GetComponent<ItemTile>();
-                    if (itemTileScript.CurrentItem == null)
-                    {
-                        itemTileScript.CurrentItem = Instantiate(itemPrefabTEST, hitGO.transform.position, itemPrefabTEST.transform.rotation, null);
-                    }
+                itemTileScriptForHit = hitForTile.collider.GetComponent<ItemTile>();
 
+                if (itemTileScriptForHit.CurrentItem == null)
+                {
+                    itemSelectedTower.SetActive(true);
+                    if (Input.GetButton("Fire1"))
+                    {
+                        itemTileScriptForHit.CurrentItem = Instantiate(itemPrefabTestTower, hitGO.transform.position + Vector3.up * 3, hitGO.transform.rotation, null);
+                    }
+                    
                 }
+                else
+                {
+                    itemSelectedTower.SetActive(false);
+                    if (Input.GetButton("Fire2"))
+                    {
+                        Destroy(itemTileScriptForHit.CurrentItem);
+                        itemTileScriptForHit.CurrentItem = null;
+                    }
+                }
+                itemSelectedTower.transform.position = hitGO.transform.position;
+                itemSelectedTower.transform.rotation = hitGO.transform.rotation;
+                itemSelectedTower.transform.position += Vector3.up * 3;
             }
             else
             {
-                itemSelected.SetActive(false);
+                itemSelectedTower.SetActive(false);
+            }
+
+            if (hitGO.CompareTag("TilePath"))
+            {
+                itemTileScriptForHit = hitForTile.collider.GetComponent<ItemTile>();
+
+                if (itemTileScriptForHit.CurrentItem == null)
+                {
+                    itemSelectedPath.SetActive(true);
+                    if (Input.GetButton("Fire1"))
+                    {
+                        itemTileScriptForHit.CurrentItem = Instantiate(itemSelectedPath, hitGO.transform.position + Vector3.up * 3, hitGO.transform.rotation, null);
+                    }
+
+                }
+                else
+                {
+                    itemSelectedPath.SetActive(false);
+                    if (Input.GetButton("Fire2"))
+                    {
+                        Destroy(itemTileScriptForHit.CurrentItem);
+                        itemTileScriptForHit.CurrentItem = null;
+                    }
+                }
+                itemSelectedPath.transform.position = hitGO.transform.position;
+                itemSelectedPath.transform.rotation = hitGO.transform.rotation;
+                itemSelectedPath.transform.position += Vector3.up * 3;
+            }
+            else
+            {
+                itemSelectedPath.SetActive(false);
             }
         }
     }
