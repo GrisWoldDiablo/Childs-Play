bc5b0af3a47736d031ad44d283f02237053bc853
GrisWoldDiablo <griswoldd@hotmail.com>
2019-02-03 15:21:47 -0500

    -+* Tile/Item placement overhaul
    Added tile cursor, selection, item placement, item remove, place holder for UI just for testing, the GM script will need to be intergrated to the actual GameManagement script.

--- Child's Play GE2 Project 2019/Assets/_Alex/Scripts/Item.cs
+++ Child's Play GE2 Project 2019/Assets/_Alex/Scripts/Item.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private GameObject rangeGO;
+    [SerializeField] private string itemName;
+
+    /// <summary>
+    /// this is the base value of the Item, when the players buys this item the value is reduce 
+    /// but it will go up as this item is upgraded in order to increase its resell value.
+    /// </summary>
+    [SerializeField] private float value = 1; 
 
     public GameObject RangeGO { get => rangeGO; set => rangeGO = value; }
+    public float Price { get => value; }
+    public string ItemName { get => itemName; }
 
     // Start is called before the first frame update
     void Awake()
--- Child's Play GE2 Project 2019/Assets/_Alex/Scripts/ItemTile.cs
+++ Child's Play GE2 Project 2019/Assets/_Alex/Scripts/ItemTile.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    Tower,
+    Barrier,
+    Unavailable
+}
+
 public class ItemTile : MonoBehaviour
 {
+    [SerializeField] private TileType tileType;
     [SerializeField] private GameObject currentItem;
 
     public GameObject CurrentItem { get => currentItem; set => currentItem = value; }
+    public TileType TileType { get => tileType; }
 
+    private AlexGMTest alexGMTestCode;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        alexGMTestCode = GameObject.Find("AlexGMTest").GetComponent<AlexGMTest>();
     }
 
     // Update is called once per frame
@@ -20,8 +31,34 @@ public class ItemTile : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Called every frame when the cursor is above the tile.
+    /// </summary>
     private void OnMouseOver()
     {
         Debug.Log(this.gameObject.name);
+        if (Input.GetButton("Fire1") && tileType != TileType.Unavailable)
+        {
+            alexGMTestCode.TileSelection(this);
+        }
+    }
+
+    /// <summary>
+    /// Called once when the cursor exit the tile.
+    /// </summary>
+    private void OnMouseExit()
+    {
+        alexGMTestCode.TileSelectionCursor.SetActive(false);
+    }
+
+    /// <summary>
+    /// Called once when the cursor enter the tile.
+    /// </summary>
+    private void OnMouseEnter()
+    {
+        if (tileType != TileType.Unavailable)
+        {
+            alexGMTestCode.ShowCursorOnTile(alexGMTestCode.TileSelectionCursor, this); 
+        }
     }
 }
