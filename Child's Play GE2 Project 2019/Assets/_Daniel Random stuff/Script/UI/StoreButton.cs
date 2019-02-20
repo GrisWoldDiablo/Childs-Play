f1ed3039a0e6ebc3136b35269cc517639ed5e3cc
Daniel Cosme <cosmedaniel8@gmail.com>
2019-02-20 12:59:20 -0500

    * Small Fix
    + Added Slider for food

--- Child's Play GE2 Project 2019/Assets/_Daniel Random stuff/Script/UI/StoreButton.cs
+++ Child's Play GE2 Project 2019/Assets/_Daniel Random stuff/Script/UI/StoreButton.cs
@@ -6,14 +6,10 @@ public class StoreButton : MonoBehaviour
 {
     [SerializeField] public GameObject itemType;
     private Item itemScript;
-    private string _name;
-    private string _desc;
-    private int _cost;
+
     private int _myIndex;
 
-    public string Name { get => _name; }
-    public string Desc { get => _desc;}
-    public int Cost { get => _cost;}
+ 
     public int MyIndex { get => _myIndex; set => _myIndex = value; }
 
     // Start is called before the first frame update
@@ -21,9 +17,6 @@ public class StoreButton : MonoBehaviour
     {
         itemScript = itemType.gameObject.GetComponent<Item>();
         _myIndex = itemScript.IndexInGM;
-        //_name = itemScript.ItemName;
-        //_desc = itemScript.ItemDescription;
-        //_cost = itemScript.Value;
     }
 
     // Update is called once per frame
