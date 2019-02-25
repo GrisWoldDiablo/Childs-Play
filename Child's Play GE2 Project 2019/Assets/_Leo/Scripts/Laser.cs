fb2fa9a3fcf7e4a2e537476c9d4b0c183d3fa761
Medina <medina.engineering@outlook.com>
2019-02-25 14:55:44 -0500

    Commit19(Leo)
    +Added Laser Machanics using LineRenderer instead of Particles
    *Working on Laser Particle Effects

---
+++ Child's Play GE2 Project 2019/Assets/_Leo/Scripts/Laser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Tower))]
+public class Laser : Projectile
+{
+    //new void Update()
+    //{
+    //    base.Update();
+    //    HittingTarget();
+    //}
+
+    [SerializeField]
+    private LineRenderer _lineRendererComponent;
+
+    private Tower _tower;
+
+    private void Start()
+    {
+        _tower = this.GetComponent<Tower>();
+    }
+
+    public void FireLaserBeam()
+    {
+        //TODO: DAMAGE INPUT
+
+
+        //GRAPHICS PART
+        _lineRendererComponent.enabled = true;
+
+        _lineRendererComponent.SetPosition(0, _tower.GetProjectileSpawnPoint.position);
+        _lineRendererComponent.SetPosition(1, _tower.GetTowerTarget.position);
+    }
+}
