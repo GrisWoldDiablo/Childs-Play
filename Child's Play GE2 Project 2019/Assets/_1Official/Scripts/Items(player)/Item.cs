330ba5a42136d583ef8cac44b5eb5c3f97626e82
GrisWoldDiablo <griswoldd@hotmail.com>
2019-03-23 21:50:52 -0400

    -+* Added UpgradeItemInfo Script
    Now when you highlight/select the buy button there is a script, "UpgradeItemInfo", that detects this  and can do stuff, for now it shows the RangeUpgrade for the next level. and its on the playmenu prefab on the buy button.
    Added a RangeUpgrade GameObject to all the Towers prefab.
    Added a RangeUpgrade material for it.
    Improved the range scaling methods, now a broadcast from the towerscript.
    Had to change the prefab from the Magnifyer Placeholder as it was not a variant from the proper prefab. Save that to the GameManager prefab.
    Deleted a test script.
    Moved SimpleDisable.cs to Obsolete.

--- Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Items(player)/Item.cs
+++ Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Items(player)/Item.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : Player
 {
     [SerializeField] private GameObject rangeGO;
+    [SerializeField] private GameObject rangeGOUpgrade;
     [SerializeField] private string itemName;
     [SerializeField, Multiline] private string itemDescription;
     [SerializeField] private int indexInGM;
@@ -20,9 +21,10 @@ public class Item : Player
     public GameObject RangeGO { get => rangeGO; set => rangeGO = value; }
     public int Value { get => value; set => this.value = value; }
     public string ItemName { get => itemName; }
-    public string ItemDescription { get => itemDescription; private set => itemDescription = value; }
-    public int IndexInGM { get => indexInGM; private set => indexInGM = value; }
+    public string ItemDescription { get => itemDescription; }
+    public int IndexInGM { get => indexInGM;}
     public Item UpgradeVersion { get => upgradeVersion; }
+    public GameObject RangeGOUpgrade { get => rangeGOUpgrade; set => rangeGOUpgrade = value; }
 
     // Start is called before the first frame update
     void Awake()
@@ -32,7 +34,33 @@ public class Item : Player
         //    rangeGO.SetActive(false);
         //}
 
-        indexInGM = 0;
+        //indexInGM = 0;
         //itemDescription = "This is a placeholder description";
+    }
+
+    private void Start()
+    {
+        if (rangeGOUpgrade != null)
+        {
+            rangeGOUpgrade.SetActive(false);
+            if (upgradeVersion != null)
+            {
+                SetRangeUPScale(upgradeVersion.GetComponent<Tower>().Tower_SO.range);
+            }
+        }
+    }
+
+    public void SetRangeScale(float scaleV3)
+    {
+        RangeGO.transform.localScale = Vector3.one * scaleV3 * 2.0f;
+        RangeGO.transform.localScale = new Vector3(RangeGO.transform.localScale.x,
+            RangeGO.transform.localScale.y * 0.33f, RangeGO.transform.localScale.z);
+    }
+
+    public void SetRangeUPScale(float scaleV3)
+    {
+        rangeGOUpgrade.transform.localScale = Vector3.one * scaleV3 * 2.0f;
+        rangeGOUpgrade.transform.localScale = new Vector3(rangeGOUpgrade.transform.localScale.x,
+            rangeGOUpgrade.transform.localScale.y * 0.33f, rangeGOUpgrade.transform.localScale.z);
     }
 }
