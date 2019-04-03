dd8e7737101f7414fc83eb884ea58d5216acdde9
Medina <medina.engineering@outlook.com>
2019-04-03 07:36:38 -0400

    Commit29(Leo)
    +Turret now locks on Target

--- Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Items(player)/Tower.cs
+++ Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Items(player)/Tower.cs
@@ -83,10 +83,17 @@ public class Tower : MonoBehaviour
             }
             float distanceToEnemy = Vector3.Distance(this.transform.position, enemy.transform.position);
 
-            if(distanceToEnemy < shortestDistance && distanceToEnemy >= innerRadius)
+            //if(distanceToEnemy < shortestDistance && distanceToEnemy >= innerRadius)
+            //{
+            //    shortestDistance = distanceToEnemy;
+            //    nearestEnemy = enemy;                
+            //}
+            
+            if (distanceToEnemy < range)
             {
                 shortestDistance = distanceToEnemy;
                 nearestEnemy = enemy;
+                break;
             }
         }
 
