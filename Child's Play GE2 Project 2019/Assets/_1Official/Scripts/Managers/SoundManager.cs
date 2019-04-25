9edb7be85c6404309cb27256ee3bebb491b6c0b7
GrisWoldDiablo <griswoldd@hotmail.com>
2019-04-25 12:46:22 -0400

    *Missed a file from previous commit

--- Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Managers/SoundManager.cs
+++ Child's Play GE2 Project 2019/Assets/_1Official/Scripts/Managers/SoundManager.cs
@@ -57,24 +57,41 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioSource _ambientMusic;
     [SerializeField] private AudioSource _uiSfx;
 
+    /// <summary>
+    /// Play a sound once
+    /// </summary>
+    /// <param name="s">what sound to play</param>
+    /// <param name="vol"> which volume to play the sound at</param>
     public void PlaySoundOneShot(Sound s , float vol = 1f)
     {
         _uiSfx.PlayOneShot(GetAudioClip(s), vol);
     }
 
+    /// <summary>
+    /// play the sound of a button click
+    /// </summary>
     public void PlaySoundButton()
     {
         _uiSfx.PlayOneShot(GetAudioClip(Sound.OnButtonClick));
     }
 
-     public void PlaySoundOneShotShopOnly(Sound s, float vol = 1f)
-     {
-            if (!_uiSfx.isPlaying)
-            {
-                _uiSfx.PlayOneShot(GetAudioClip(s), vol);
-            }
-     }
+    /// <summary>
+    /// Play a sound once, Shop specific
+    /// </summary>
+    /// <param name="s">what sound to play</param>
+    /// <param name="vol"> which volume to play the sound at</param>
+    public void PlaySoundOneShotShopOnly(Sound s, float vol = 1f)
+    {
+        if (!_uiSfx.isPlaying)
+        {
+            _uiSfx.PlayOneShot(GetAudioClip(s), vol);
+        }
+    }
 
+    /// <summary>
+    /// Choose a music to play
+    /// </summary>
+    /// <param name="level">Which level is loaded</param>
     public void PlayMusic(int level)
     {
         switch (level)
@@ -104,12 +121,19 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// stop the music that is playing
+    /// </summary>
     public void StopMusic()
     {
         _ambientMusic.Stop();
     }
 
-
+    /// <summary>
+    /// Return the audio clip that is store
+    /// </summary>
+    /// <param name="s">what clip to get</param>
+    /// <returns>the audioclip found</returns>
     public AudioClip GetAudioClip(Sound s)
     {
         foreach (SoundAudioclip audioClip in _mySounds)
@@ -122,8 +146,7 @@ public class SoundManager : MonoBehaviour
         Debug.LogError("Sound " + s + " not found");
         return null;
     }
-
-  
+    
 }
 
 
